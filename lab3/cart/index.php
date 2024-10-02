660d5fbf45b6958034432b3f6a567d346d0d5393
VitaliyPanchishniy <144204960+VitaliyPanchishniy@users.noreply.github.com>
2024-10-02 13:07:09 +0300

    Update index.php

--- lab3/cart/index.php
+++ lab3/cart/index.php
@@ -1,17 +1,14 @@
 <?php
 session_start();
 
-// Додавання товарів до сесії
 if (isset($_POST['product'])) {
     $_SESSION['cart'][] = $_POST['product'];
 }
 
-// Збереження попередніх покупок у cookie
 if (isset($_SESSION['cart'])) {
     setcookie('previous_cart', serialize($_SESSION['cart']), time() + 7 * 24 * 60 * 60);
 }
 
-// Виведення товарів з корзини
 if (isset($_SESSION['cart'])) {
     echo "<h2>Товари в корзині:</h2><ul>";
     foreach ($_SESSION['cart'] as $item) {
@@ -20,7 +17,6 @@ if (isset($_SESSION['cart'])) {
     echo "</ul>";
 }
 
-// Виведення попередніх покупок з cookie
 if (isset($_COOKIE['previous_cart'])) {
     $previous_cart = unserialize($_COOKIE['previous_cart']);
     echo "<h2>Попередні покупки:</h2><ul>";
